26e42d7a3f5df94b45d463121ee6c09d86461cfc
joshbenandiel <joshjacinto22@gmail.com>
2021-12-07 20:27:20 +0800

    aericainthehouse

--- src/components/SearchItems.jsx
+++ src/components/SearchItems.jsx
@@ -7,24 +7,149 @@ const SearchItems = ({searchTerm}) => {
 
 
   const productsData = useSelector((state) => state.product.products);
+  const [sortMenu , setSortMenu] = useState(true);
+  const [viewList , setViewList] = useState(false)
+  const [viewGrid , setViewGrid] = useState(true)
+
+  let sortItems =  [
+    {
+      sortText: "Best Selling"
+    },
+    {
+      sortText: "Price, low to high"
+    },
+    {
+      sortText: "Price, high to low"
+    },
+    {
+      sortText: "Alphabetically, A-Z"
+    },
+    {
+      sortText: "Alphabetically, Z-A"
+    }
+    
+    
+  ]
+
+  const [sortContent, setSortContent] = useState(sortItems[0].sortText)
+  
 
   return (
-    <>
-    <div className="search-items-container">
-      {productsData.map((item,key) => {
-          return (
-            <div className="search-items-wrapper">
-              <div className="search-items"key={key}>
-                  <img src={item.img} alt="item-picture"/>
-                  <h5>₱ {item.price}</h5>
-                  <h5>{item.series}</h5>
-                  <Button className="mt-3" variant="contained">Add to cart</Button>
+      <div className="search-items-wrapper">
+          <div className="search-container-items container">
+            <div className="search-row row">
+              <div className="search-show-product col">
+                <p>Showing {productsData.length} products</p>
               </div>
-            </div>      
-          )
-      })}
-    </div>
-    </>
+              <div className="col-6">
+                <div className="search-items-header"> 
+                  {sortMenu ?
+                    <div className="search-items-sort">
+                      <button
+                      onClick={() => {
+                      setSortMenu(!sortMenu)
+                      console.log(sortItems);
+                      }} 
+                      style={{background: "transparent", border: "none"}}>
+                      <div className="sort-items-menu">
+                        <p className="m-0">Sort by: {sortContent}
+                        <i class="far fa-chevron-down fa-xs"></i>
+                        </p>
+                      </div>
+                      </button>
+                    </div> :                                  
+                   <div className="search-items-sort">
+                      <button
+                      onClick={() => {
+                        setSortMenu(!sortMenu)
+                      }} 
+                      style={{background: "transparent", border: "none"}}>
+                      <div className="sort-items-menu">
+                        <p className="m-0">Sort by: {sortContent}
+                        <i class="far fa-chevron-up fa-xs"></i>
+                        </p>
+                      </div>
+                      </button>
+                      <div className="sortItem-open">
+                        <div className="sort-item-wrapper">
+                          {sortItems.map( item => {
+                            return (
+                              <div>
+                                  <p
+                                  onClick={() => {
+                                      setSortContent(item.sortText);
+                                      setSortMenu(!sortMenu)
+                                      if (item.sortText) {
+
+                                      }
+                                  }} 
+                                  className="sort-items-content p-2">{item.sortText}</p>
+                              </div>
+                            )
+                            
+                          })}
+                        </div>
+                      </div>
+                    </div> } 
+                    
+                </div>
+              </div>
+              <div className="col">          
+                <div className="view-wrapper">
+                  <p className="m-0">View</p>
+                    <button
+                    onClick={() => {
+                      setViewGrid(true)
+                      setViewList(false)
+                    }} 
+                    className={viewGrid ? "view-grid m-2" : "m-2"} 
+                    style={{background: "transparent", border: "none"}}><i class="fas fa-th"></i></button>
+                    <button
+                    onClick={() => {
+                      setViewList(true)
+                      setViewGrid(false)
+                      
+                    }} 
+                    className={viewList && "view-list"}
+                    style={{background: "transparent", border: "none"}}><i class="fas fa-list"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={viewGrid && "search-items-container"}>
+            {productsData.map((item,key) => {
+                return (
+                  <>
+                  {viewList ?                     
+                    <div className="search-items-container-list" key={key}>
+                      <div className="container">
+                        <div className="row">
+                          <div className="view-list-img col">
+                            <img src={item.img} alt="item-picture"/>
+                          </div>
+                          <div className="view-list-text col-6">
+                            <h5 style={{color: "blue"}}>₱ {item.price}</h5>
+                            <h5>{item.series}</h5>
+                          </div>
+                          <div className="view-list-button col">
+                            <Button className="mt-3" variant="contained">Add to cart</Button>
+                            <Button className="mt-3" variant="outlined">Quick view</Button>
+                          </div>
+                        </div>
+                      </div>
+                    </div> 
+                  :  
+                    <div className="search-items" key={key}>
+                        <img src={item.img} alt="item-picture"/>
+                        <h5 style={{color: "blue"}}>₱ {item.price}</h5>
+                        <h5>{item.series}</h5>
+                        <Button className="mt-3" variant="contained">Add to cart</Button>
+                    </div>}            
+                  </>     
+                  )
+              })}
+          </div>
+        </div>
     
   )
 }
